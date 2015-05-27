1a539562477bf65068db7a5200bb3d6bee90b931
Ben Wedin <wedinb@carleton.edu>
2015-05-27 17:07:55 -0500

    poking around in fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Group terrainGroup;
+    private TerrainModel terrainModel;
+
+    public Controller() {
+    }
+
+    public void initialize() {
+        for (Node node : this.terrainGroup.getChildren()) {
+            System.out.println("hi");
+            //TerrainView terrain_view = (TerrainView)node;
+            //this.terrainModel = new TerrainModel();
+            //terrain_view.setTerrainModel(this.terrainModel);
+            //terrain_view.update();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = (Parent)loader.load();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 800));
         primaryStage.show();
     }
 
